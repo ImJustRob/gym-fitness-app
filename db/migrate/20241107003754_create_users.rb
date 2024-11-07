1daa56cae23bbafd2aaa26aee55e66c647012061
ImJustRob <robertalothian@hotmail.com>
2024-11-06 19:56:02 -0500

    models for user, exercise and routine

--- db/migrate/20241107003754_create_users.rb
+++ db/migrate/20241107003754_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password_diguest
+      t.string :password_digest
       t.string :image_url
 
       t.timestamps
